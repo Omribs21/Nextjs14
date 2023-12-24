582ce96beec5de0165d87bbb36509a603c388f52
Omribs21 <omri2361@gmail.com>
2023-12-24 22:38:40 +0200

    final additions to webhooks

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        serverActions:true,
-        mdxRs:true,
-        serverComponentsExternalPackages:['mongoose']
-    }
-}
+  experimental: {
+    serverActions: true,
+    mdxRs: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  // to allow getting image of the user to show near is name where the question is asked
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
