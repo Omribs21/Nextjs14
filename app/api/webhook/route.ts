2e2fe6628684838a98a3fdda4568ca9b5148ece0
Omribs21 <omri2361@gmail.com>
2023-12-24 21:44:13 +0200

    add api webhook path

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -8,7 +8,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   // add webhook to.env
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
@@ -54,6 +54,7 @@ export async function POST(req: Request) {
 
   // Get the ID and type
   const eventType = evt.type;
+  console.log({eventType})
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
