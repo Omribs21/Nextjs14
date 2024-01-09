95c47fe7e1f47c62fb2b618f7bc15230c224c0a5
Omribs21 <omri2361@gmail.com>
2024-01-09 21:38:52 +0200

    show top tags and questions on the right sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,26 +2,12 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import RenderTag from "./RenderTag";
-const howQuestions = [
-  {
-    _id: "1",
-    title:
-      "Best practices for data fetching in a Next.js application with Server-Side Rendering ",
-  },
-  { _id: "2", title: "Is it only me or the font is bolder than necessary? " },
-  { _id: "3", title: "Can I get the course for free? " },
-  { _id: "4", title: "Redux Toolkit Not Updating State as Expected" },
-  { _id: "5", title: "Async/Await Function Not Handling Errors Properly" },
-];
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
-const popularTags = [
-  { _id: "1", name: "javascript", totalQuestions: 5 },
-  { _id: "2", name: "react", totalQuestions: 1 },
-  { _id: "3", name: "next", totalQuestions: 3 },
-  { _id: "4", name: "vue", totalQuestions: 4 },
-  { _id: "5", name: "redux", totalQuestions: 5 },
-];
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
   return (
     <section
       className="background-light900_dark200 light-border sticky right-0 top-0
@@ -31,7 +17,7 @@ const RightSidebar = () => {
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px]">
-          {howQuestions.map((question) => {
+          {hotQuestions.map((question) => {
             return (
               <Link
                 href={`/question/${question._id}`}
@@ -56,12 +42,12 @@ const RightSidebar = () => {
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
-          {popularTags.map((tag) => (
+          {popularTags.map((tag: any) => (
             <RenderTag
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
